938010759ca99e711586306f98ca26b5f6a37dc1
AAIDEV <awbkerbal@gmail.com>
2016-03-31 10:15:24 -0700

    Create sf.js

---
+++ sf.js
@@ -0,0 +1,8 @@
+function simpleFooter(sfename, sfdebug) {
+  //REQUIRES JQUERY!!!
+  var sfdHeight = $( document ).outerHeight();
+  if (sfdebug == true) {
+    console.log(sfdHeight.tostring);
+  }
+  document.getElementById(sfename).style.top = ((mama).toString() + "px");
+}
